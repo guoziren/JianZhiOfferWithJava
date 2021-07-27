38a0445e08ebe52923c688868f2bc547e0f0c702
guoziren <234917515@qq.com>
2021-07-27 12:43:35 +0800

    增加一个构造函数

--- src/main/java/com/ustc/common/ListNode.java
+++ src/main/java/com/ustc/common/ListNode.java
@@ -12,4 +12,9 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
